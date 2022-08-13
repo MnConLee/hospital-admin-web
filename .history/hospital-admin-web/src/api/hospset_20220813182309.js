a821610cca562f66ed6a47da00138fdcdf509cc7
MnConLee <2360078968@qq.com>
2022-08-13 18:55:39 +0800

    first commit

---
+++ .history/hospital-admin-web/src/api/hospset_20220813182309.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+export default {
+  //医院设置列表
+  getHospSetList(current,limit,searchObj) {
+    return request ({
+      url: `/admin/hosp/hospitalSet/findAll/${current}/${limit}`,
+      method: 'get',
+      data: searchObj
+    })
+  },
+  //删除医院设置
+  deleteHospSet(id) {
+    return request ({
+      url: `/admin/hosp/hospitalSet/${id}`,
+      method: 'delete'
+    })
+  },
+  //批量删除
+  batchRemoveHospSet(idList) {
+    return request ({
+      url: `/admin/hosp/hospitalSet/batchRemove`,
+      method: 'delete',
+      data: idList
+    })
+  },
+  //锁定和取消锁定
+  lockHospSet(id,status) {
+    return request ({
+      url: `/admin/hosp/hospitalSet/lockHospitalSet/${id}/${status}`,
+      method: 'put'
+    })
+  },
+  //添加医院设置
+  saveHospSet(hospitalSet) {
+    return request ({
+      url: `/admin/hosp/hospitalSet/saveHospitalSet`,
+      method: 'post',
+      data: hospitalSet
+    })
+  },
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# hospital-admin-web
+医院预约挂号系统后台前端
