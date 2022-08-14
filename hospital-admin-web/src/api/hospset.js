5051c232702c7770b2a7b41ce43edfa4e22d42e7
MnConLee <2360078968@qq.com>
2022-08-14 13:33:25 +0800

    新增医院设置添加和编辑

--- hospital-admin-web/src/api/hospset.js
+++ hospital-admin-web/src/api/hospset.js
@@ -39,6 +39,21 @@ export default {
       data: hospitalSet
     })
   },
+  // 医院设置id查询
+  getHospSet(id){
+    return request ({
+      url: `/admin/hosp/hospitalSet/getHospSet/${id}`,
+      method: 'get',
+    })
+  },
+  // 修改医院设置
+  updateHospSet(hospitalSet){
+    return request ({
+      url: `/admin/hosp/hospitalSet/updateHospSet`,
+      method: 'post',
+      data: hospitalSet
+    })
+  },
 
 
 }
