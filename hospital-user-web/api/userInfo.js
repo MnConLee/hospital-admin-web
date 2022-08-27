16a763b78f7ffe33e7c2b60561cea45873e0830d
MnConLee <2360078968@qq.com>
2022-08-27 16:04:43 +0800

    新增短信登录

--- hospital-user-web/api/userInfo.js
+++ hospital-user-web/api/userInfo.js
@@ -2,28 +2,28 @@ import request from '@/utils/request'
 
 const api_name = `/api/user`
 
-export default {
-    //邮箱登录
-    login(userInfo) {
-        return request({
-            url: `${api_name}/login`,
-            method: `post`,
-            data: userInfo
-        })
-    },
-    //用户认证
-    saveUserAuth(userAuth){
-        return request({
-            url: `${api_name}/auth/userAuth`,
-            method: `post`,
-            data:userAuth
-        })
-    },
-    //根据userid获取用户信息
-    getUserInfo(){
-        return request({
-            url: `${api_name}/auth/getUserInfo`,
-            method: `get`,
-        })
-    }
+//用户登录
+export default {
+  login(userInfo) {
+    return request({
+      url: `${api_name}/login`,
+      method: `post`,
+      data: userInfo
+    })
+  },
+
+  getUserInfo() {
+    return request({
+      url: `${api_name}/auth/getUserInfo`,
+      method: `get`
+    })
+  },
+
+  saveUserAuah(userAuah) {
+    return request({
+      url: `${api_name}/auth/userAuah`,
+      method: 'post',
+      data: userAuah
+    })
+  }
 }
